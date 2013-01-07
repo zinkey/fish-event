75ba48bdc2a4e8ff581c84cc57f64abf8cbdc79d
zinkey <yaya@uloveit.com.cn>
2013-01-07 11:06:44 +0800

    yaya

--- fish-event.js
+++ fish-event.js
@@ -5,16 +5,18 @@
 	};
 	var num=0;
 	window.FishEvent = function(fn){
-		var tag_fn = num++;
+		var tag_fn = (num++).toString(36);
 		return function(){
-			var tag_args = num++;
+			var tag_args = "-1";
 			map.fn[tag_fn] = fn;
-			map.args[tag_args] = arguments;
+			if (arguments.length!=0){
+				tag_args = (num++).toString(36);
+				map.args[tag_args] = Array.prototype.slice.call(arguments);
+			}
 			return "FishEvent.event('"+tag_fn+"','"+tag_args+"')";
 		}
 	};
 	FishEvent.event = function(tag_fn,tag_args){
-		var target = event.target||event.srcElement;
-		map.fn[tag_fn].apply(target,map.args[tag_args]);
+		map.fn[tag_fn].apply(event.target||event.srcElement,map.args[tag_args]);
 	};
 })();
